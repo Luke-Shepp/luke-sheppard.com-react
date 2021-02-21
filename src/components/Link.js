e27a716aa4d503a8919dd938aec1bc86216a83a1
Luke Sheppard <luke@luke-sheppard.com>
2021-02-21 15:56:55 +0000

    Open links in new tab

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Link = ({ href, children }) => {
     return (
-        <a href={ href } className="link">
+        <a href={ href } className="link" target="_blank" rel="noreferrer">
             { children }
         </a>
     );
